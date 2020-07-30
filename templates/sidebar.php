7d8fca94f24d6423fad2d10ebeb42d6d5104a5d5
Tobiloba <oomolara40@gmail.com>
2020-07-30 16:19:57 +0100

    Templates

---
+++ templates/sidebar.php
@@ -0,0 +1,41 @@
+<div class="left side-menu">
+    <div class="slimscroll-menu" id="remove-scroll">
+
+        <!--- Sidemenu -->
+        <div id="sidebar-menu">
+            <!-- Left Menu Start -->
+            <ul class="metismenu" id="side-menu">
+                <li class="menu-title">Overview</li>
+                <li>
+                    <a href="index.php" class="waves-effect">
+                        <i class="ion ion-md-speedometer"></i><span class="badge badge-success badge-pill float-right">2</span> <span> Dashboard </span>
+                    </a>
+                </li>
+
+                <li class="menu-title">Apps</li>
+                
+                <li>
+                    <a href="documentation.php" class="waves-effect waves-light"><i class="ion ion-md-cloud-upload"></i><span> Upload Documentation </span></a>
+                </li>
+                <li>
+                    <a href="publication.php" class="waves-effect waves-light"><i class="ion ion-md-cloud-upload"></i><span> Upload Publication </span></a>
+                </li>
+                <li>
+                    <a href="profile.php" class="waves-effect waves-light"><i class="ion ion-md-cloud-upload"></i><span> Create Profile </span></a>
+                </li>
+
+                <li>
+                    <a href="calendar.php" class="waves-effect"><i class="ion ion-md-calendar"></i><span> Calendar </span></a>
+                </li>
+
+                
+            </ul>
+
+        </div>
+        <!-- Sidebar -->
+        <div class="clearfix"></div>
+
+    </div>
+    <!-- Sidebar -left -->
+
+</div>
